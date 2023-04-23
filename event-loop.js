6e1261e21fd5f1df8756933f5596bf6fe10fe679
Shubham Singh <shubham2101@gmail.com>
2023-04-23 20:25:26 +0530

    nextTick

--- event-loop.js
+++ event-loop.js
@@ -9,6 +9,7 @@ fs.readFile("test-file.txt", () => {
   setTimeout(() => console.log("SetTimeout 2 finished"), 0);
   setTimeout(() => console.log("SetTimeout 3 finished"), 3000);
   setImmediate(() => console.log("Immediate 2 finished"));
+  process.nextTick(() => console.log("nextTick executed"))
 });
 
 console.log("Hello from top level code");
